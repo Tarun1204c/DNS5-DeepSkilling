bd78581d8bb713c7b072e10b328b64c550df6fb3
Tarun Pandav <[valid-email]>
2026-07-06 15:27:05 +0530

    condition sql

---
+++ Week2/Day1-PLSQL/src/03_Conditions.sql
@@ -0,0 +1,31 @@
+-- PL/SQL Conditions
+-- IF-THEN-ELSE and CASE statements
+
+DECLARE
+    v_marks NUMBER := 75;
+    v_grade VARCHAR2(10);
+
+BEGIN
+    -- IF-THEN-ELSE
+    IF v_marks >= 90 THEN
+        v_grade := 'A';
+    ELSIF v_marks >= 75 THEN
+        v_grade := 'B';
+    ELSIF v_marks >= 60 THEN
+        v_grade := 'C';
+    ELSE
+        v_grade := 'F';
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('Marks: ' || v_marks);
+    DBMS_OUTPUT.PUT_LINE('Grade: ' || v_grade);
+
+    -- CASE Statement
+    CASE v_grade
+        WHEN 'A' THEN DBMS_OUTPUT.PUT_LINE('Excellent!');
+        WHEN 'B' THEN DBMS_OUTPUT.PUT_LINE('Good!');
+        WHEN 'C' THEN DBMS_OUTPUT.PUT_LINE('Average!');
+        ELSE DBMS_OUTPUT.PUT_LINE('Fail!');
+    END CASE;
+
+END;
